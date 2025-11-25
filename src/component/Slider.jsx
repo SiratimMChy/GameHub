d548a187011106b60bc79c1ab8503bad3fca551a
Tuha chy <siratimmustakimchowdhury123@gmail.com>
2025-11-25 21:11:29 +0600

    Update slider Content

--- src/component/Slider.jsx
+++ src/component/Slider.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import img1 from '../assets/img1.jpg'
+import img2 from '../assets/img2.jpg'
+import img3 from '../assets/img3.jpg'
+import img4 from '../assets/img4.png'
 const Slider = () => {
     return (
         <div>
-            
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+                <SwiperSlide><img className='w-full h-[450px]'  src={img1} alt="" /></SwiperSlide>
+                <SwiperSlide><img className='w-full h-[450px]' src={img2} alt="" /></SwiperSlide>
+                <SwiperSlide><img className='w-full h-[450px]' src={img3} alt="" /></SwiperSlide>
+                <SwiperSlide><img className='w-full h-[450px]' src={img4} alt="" /></SwiperSlide>
+            </Swiper>
         </div>
     );
 };
