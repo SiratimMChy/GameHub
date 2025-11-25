3cc22f68e272ca75ec7bf70f296ce30b177283e5
Tuha chy <siratimmustakimchowdhury123@gmail.com>
2025-11-25 20:44:18 +0600

    Update Home & RootLayout Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Slider from '../component/Slider';
+import PopularGames from '../component/PopularGames';
 
 const Home = () => {
     return (
         <div>
-            
+            <title>Home</title>
+            <Slider/>
+            <PopularGames/>
         </div>
     );
 };
--- src/root/Rootlayout.jsx
+++ src/root/Rootlayout.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Outlet } from 'react-router';
+import Navbar from '../component/Navbar';
+import Footer from '../component/Footer';
 
 const Rootlayout = () => {
     return (
         <div>
-            
+            <Navbar/>
+            <Outlet/>
+            <Footer/>
         </div>
     );
 };
